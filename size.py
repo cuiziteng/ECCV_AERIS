741e3801b7d4a399bac35833cedfa024073969c0
Cui Ziteng <55396225+cuiziteng@users.noreply.github.com>
2023-05-11 23:06:55 +0800

    Delete size.py

--- size.py
+++
@@ -1,8 +0,0 @@
-import os
-
-import cv2
-import numpy as np
-
-for file in os.listdir(r'/data/unagi0/cui_data/coco/val2017'):
-    img = cv2.imread('/data/unagi0/cui_data/coco/val2017/'+file)
-    print(img.shape)
