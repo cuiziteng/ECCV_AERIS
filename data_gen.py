4ece5d843a1e27309342553bd2f409a274acb749
Cui Ziteng <55396225+cuiziteng@users.noreply.github.com>
2023-06-13 00:43:36 +0900

    Update data_gen.py

--- data_gen.py
+++ data_gen.py
@@ -2,9 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn.functional import interpolate
 
-import kornia
-from kornia.filters import gaussian_blur2d, motion_blur, filter2d
-import sys
+from kornia.filters import filter2d
 
 from kernel import isotropic_Gaussian, anisotropic_Gaussian
 
@@ -20,30 +18,22 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--source', type=str, default=r'/data/unagi0/cui_data/coco/val2017',
+parser.add_argument('--coco_path', type=str, default=r'/data/unagi0/cui_data/coco',
                     help='the place you store COCO validation dataset')
-parser.add_argument('--target', type=str, default=r'/data/unagi0/cui_data/coco/val2017_deg',
-                    help='the place to store the output images')
-
-# parser.add_argument('--with_low', type=bool, default=True)
+parser.add_argument('--data_type', type=str, default=r'val',
+                    help='choose train or val')
 opt = parser.parse_args()
-print(opt)
 
+image_path = os.path.join(opt.coco_path, '%s2017'%opt.data_type)
+label_path = os.path.join(opt.coco_path, 'annotations', 'instances_%s2017.json'%opt.data_type)
 device = 'cuda:0'
-
-def mkdir(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
 
 
 def mkdirs(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
-
-mkdirs(opt.target)
-
-# noise level: sigma ~ (5, 50)
+# Random Gaussian Noise, noise level: sigma ~ (5, 50)
 def random_noise(img):
     device = img.device
     img_hr = img.permute(2, 0, 1)  
@@ -54,7 +44,7 @@ def random_noise(img):
 
     return img_hr.squeeze(0).permute(1, 2, 0)
 
-# noise level: sigma fixed
+# Fix Gaussian Noise, noise level sigma is fixed
 def fix_noise(img, n=10.0):
     device = img.device
     img_hr = img.permute(2, 0, 1)  # change (H,W,C) to (1,C,H,W)
@@ -68,9 +58,7 @@ def fix_noise(img, n=10.0):
 def random_blur(img):
     # device = img.device
     device = img.device
-    # print(img.shape)
     img_hr = img.permute(2, 0, 1)  # change (H,W,C) to (1,C,H,W)
-    # print(img_hr.shape)
     img_hr = img_hr.unsqueeze(0)
     kernel_size = int(random.choice([7, 9, 11, 13, 15, 17, 19, 21]))
     kernel_width_iso = random.uniform(0.1, 2.4)
@@ -119,23 +107,35 @@ def fix_anisoblur(img, kernel_size=17, kernel_width_1=5, kernel_width_2 = 2.3, a
 
     return img_blur.squeeze(0).permute(1, 2, 0)
 
-def down_2(img):
-    device = img.device
+def down_sample(img, resolution=2):
     img_hr = img.permute(2, 0, 1)  # change (H,W,C) to (1,C,H,W)
     img_hr = img_hr.unsqueeze(0)
-    resolution = 2
     scale_mode = random.choice(['nearest', 'bilinear', 'bicubic'])
     img_lr = interpolate(img_hr, scale_factor=1 / resolution, mode=scale_mode)
     return img_lr.squeeze(0).permute(1, 2, 0)
 
-def down_4(img):
-    device = img.device
-    img_hr = img.permute(2, 0, 1)  # change (H,W,C) to (1,C,H,W)
-    img_hr = img_hr.unsqueeze(0)
-    resolution = 4
-    scale_mode = random.choice(['nearest', 'bilinear', 'bicubic'])
-    img_lr = interpolate(img_hr, scale_factor=1 / resolution, mode=scale_mode)
-    return img_lr.squeeze(0).permute(1, 2, 0)
+def down_json(json_file=label_path, resolution=2):
+    data = json.load(open(label_path, 'r'))
+    
+    image_dicts = data['images']
+    anno_dicts = data['annotations']
+    
+    for i_image in range(len(image_dicts)):
+        image_dicts[i_image]['width'] = int(image_dicts[i_image]['width']//resolution)
+        image_dicts[i_image]['height'] = int(image_dicts[i_image]['height']//resolution)
+    
+    for i_anno in range(len(anno_dicts)):
+        x, y, w, h = anno_dicts[i_anno]['bbox'][0], anno_dicts[i_anno]['bbox'][1], anno_dicts[i_anno]['bbox'][2], anno_dicts[i_anno]['bbox'][3]
+        anno_dicts[i_anno]['bbox'] = [int(x//resolution), int(y//resolution), int(w//resolution), int(h//resolution)]
+    
+    data_out = json.dumps(data)
+    json_store = json_file.replace('.json', 'down_%s.json'%str(resolution))
+    f = open(json_store, 'w')
+    f.write(data_out)
+    f.close()
+    
+    
+    
 
 # Random Generation a degradation dataset
 def random_deg(img):
@@ -192,25 +192,85 @@ def random_deg(img):
     return img_dr.squeeze(0).permute(1, 2, 0)
 
 
+def main(img_path, json_path, deg_type='random_blur', resolution=2):
+
+    assert deg_type in ['random_blur', 'fix_isoblur', 'fix_anisoblur',
+                        'random_noise', 'fix_noise',
+                        'resolution']
+    if deg_type != 'resolution':
+        img_out_path = img_path + deg_type
+    else:
+        img_out_path = img_path + deg_type + 'down_%s'%str(resolution)
+
+    # Radom Blurry
+    if deg_type == 'random_blur':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = random_blur(img_ts)
+            img_deg = img_deg_ts.cpu().numpy()
+            print(os.path.join(img_out_path, img_name))
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+
+    # Fix Isotropic Gaussian Blurry
+    elif deg_type == 'fix_isoblur':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = fix_isoblur(img_ts, kernel_size=17, kernel_width_iso=3.0)  # change hyper-parameter here
+            img_deg = img_deg_ts.cpu().numpy()
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+
+    # Fix Anisotropic Gaussian Blurry
+    elif deg_type == 'fix_anisoblur':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = fix_anisoblur(img_ts, kernel_size=17, kernel_width_1=5, kernel_width_2 = 2.3, angle = np.pi/3)  # change hyper-parameter here
+            img_deg = img_deg_ts.cpu().numpy()
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+
+    # Random Noise
+    elif deg_type == 'random_noise':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = random_noise(img_ts)  
+            img_deg = img_deg_ts.cpu().numpy()
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+    
+    # Fix Noise
+    elif deg_type == 'fix_noise':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = fix_noise(img_ts, n=10.0)  # change hyper-parameter here
+            img_deg = img_deg_ts.cpu().numpy()
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+    
+    # Down-Sampling, need change the label json file 
+    elif deg_type == 'resolution':
+        for i, img_name in tqdm(enumerate(os.listdir(img_path))):
+            img_full_name = os.path.join(img_path, img_name)
+            img = cv2.imread(img_full_name)/255.0
+            img_ts = torch.from_numpy(img).to(torch.device(device))
+            img_deg_ts = down_sample(img_ts, resolution)  # change hyper-parameter here
+            img_deg = img_deg_ts.cpu().numpy()
+            cv2.imwrite(os.path.join(img_out_path, img_name), img_deg * 255.0)
+            down_json(json_path, resolution)    # Change Json File
+
+
+
 
 if __name__ == "__main__":
-    old_root = opt.source
-    new_root = opt.target
-
-    for i, img_name in tqdm(enumerate(os.listdir(old_root))):
-        img_full_name = os.path.join(old_root, img_name)
-        img_save_name = os.path.join(new_root, img_name)
-        
-        img = cv2.imread(img_full_name) / 255.0
-
-        # turn numpy to tensor
-        img_ts = torch.from_numpy(img).to(torch.device(device))
-        
-        img_deg_ts = fix_isoblur(img_ts)
-        # or, you could change to other degradation generation process
-        # like: img_deg_ts = fix_anisoblur(img_ts), img_deg_ts = fix_noise(img_ts)
-
-        img_deg = img_deg_ts.cpu().numpy()
-        cv2.imwrite(img_save_name, img_deg * 255.0)
-    
-
+    '''
+    Choose Deg Type from:
+    ['random_blur', 'fix_isoblur', 'fix_anisoblur',
+    'random_noise', 'fix_noise', 'resolution']
+    '''
+    main(image_path, label_path, deg_type='random_blur')
